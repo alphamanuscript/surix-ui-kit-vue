059e0ccaed97764fedb2ef5c0e56d31c9d7f6b3f
Clément Habinshuti <hello@habbes.xyz>
2018-12-21 02:19:13 +0300

    Improve drawer and add basic form elements

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,62 @@
           Happy Feet
         </div>
         <div class="drawer-content">
-          <ul class="drawer-nav">
-            <li>My Apps</li>
-            <li>Marketplace</li>
-            <li>Settings</li>
-            <li>Manage Users</li>
-          </ul>
+          <div class="nav-group">
+            <div class="nav-group-header">
+              Current App
+            </div>
+            <div class="nav-group-content">
+              <div class="nav-item">Compose</div>
+              <div class="nav-item active">Inbox</div>
+              <div class="nav-item">Reports</div>
+            </div>
+          </div>
+          <div class="separator"></div>
+          <div class="nav-item">Apps</div>
+          <div class="nav-item">Marketplace</div>
+          <div class="separator"></div>
+          <div class="nav-item">Settings</div>
+          <div class="nav-item">Manage users</div>
         </div>
       </div>
       <div class="main">
-        <div class="main-header">
+        <div class="main-header text-extra-thin">
           New product
         </div>
         <div class="page-content">
           <div class="banner">
             
           </div>
-          <div class="container margin-top-lg">
-            <div class="panel">
+          <div class="container mt-lg">
+            <div class="panel mb-lg">
+              <div class="panel-header">
+                <div class="panel-title">
+                  Basic details
+                </div>
+                <div class="panel-subtitle">
+                  Basic information about the product
+                </div>
+              </div>
+              <div class="panel-body">
+                <!-- <div>Label</div> -->
+                <div class="field-group">
+                  <input type="text" class="text-field" placeholder="Enter name" />
+                </div>
+                <div class="field-group">
+                  <textarea class="text-field" placeholder="Enter description"></textarea>
+                </div>
+                <div class="field-group">
+                  <button class="button">Click Me</button>
+                </div>
+                <div class="field-group">
+                  <button class="button fill">Click Me</button>
+                </div>
+              </div>
+            </div>
+            <div class="panel mb-lg">
 
             </div>
-            <div class="panel">
-
-            </div>
-            <div class="panel">
+            <div class="panel mb-lg">
 
             </div>
           </div>
@@ -51,21 +83,33 @@ export default class App extends Vue {}
 <style lang="scss">
 
 $font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-$drawer-width: 256px;
-$header-height: 70px;
-$background-color: #fcfdff;
+$font-family-alt: 'Helvetica Neue', sans-serif;
+$drawer-width: 220px;
+$header-height: 60px;
+$background-color: #fafafb;
 $drawer-background-color: #27375D;
 $header-background-color: white;
 $border-color: #eaecef;
 $border-radius: 2px;
-$padding: 16px;
+$padding: 20px;
+
+$margin-y: 25px;
 
 $text-light: #D6E3FF;
 $text-color: #1D2945;
+$text-secondary: #818796;
 
 @mixin fill-viewport {
   min-width: 100vw;
   min-height: 100vh;
+}
+
+@mixin text-thin {
+  font-weight: 300;
+}
+
+@mixin text-extra-thin {
+  font-weight: 200;
 }
 
 * {
@@ -77,9 +121,20 @@ ul {
   padding: 0;
 }
 
-.margin-top-lg {
-  margin-top: 50px;
+.mt {
+  margin-top: $margin-y;
+  &-lg {
+    margin-top: $margin-y * 2;
+  }
 }
+
+.mb {
+  margin-bottom: $margin-y;
+  &-lg {
+    margin-bottom: $margin-y * 2;
+  }
+}
+
 
 html {
   @include fill-viewport();
@@ -98,6 +153,14 @@ body {
   height: 100vh;
 }
 
+.text-thin {
+  @include text-thin();
+}
+
+.text-extra-thin {
+  @include text-extra-thin;
+}
+
 .drawer-nav-layout {
   display: flex;
   flex-direction: row;
@@ -111,6 +174,7 @@ body {
     @media screen and (max-width: 500px) {
       display: none;
     }
+    z-index: 30;
   }
 
   .main {
@@ -134,7 +198,7 @@ body {
 }
 
 .main-header {
-  background-color: rgba($header-background-color, 0.7);
+  background-color: rgba($header-background-color, 0.95);
   height: $header-height;
   box-shadow: 0px 0px 3px #ced0d2;
   z-index: 20;
@@ -167,11 +231,39 @@ body {
   color: $text-light;
 }
 
-.drawer-nav {
-  li {
-    color: $text-light;
-    padding: $padding;
-    font-size: 0.9em;
+.drawer-content {
+  @include text-thin;
+  padding-top: $padding;
+  .nav-group-header {
+    text-transform: uppercase;
+    font-size: 0.75em;
+    color: #91a6d2;
+    padding: 10px $padding;
+    font-weight: 500;
+  }
+  .nav-item {
+    color: #91a6d2;
+    padding: 10px $padding;
+    font-size: 0.85em;
+    
+    &.active {
+      color: $text-light;
+      background-color: #1b3158;
+    }
+
+    &:hover {
+      background-color: #1b3158;
+    }
+  }
+
+  .nav-group {
+    .nav-item {
+      padding-left: $padding * 2;
+    }
+  }
+
+  .separator {
+    border-bottom: 1px solid #172b4e;
   }
 }
 
@@ -188,6 +280,98 @@ body {
   border: 1px solid $border-color;
   border-radius: $border-radius;
   // box-shadow: 0px 0px 3px #ced0d2;
+
+  .panel-header {
+    padding: $padding;
+    border-bottom: 1px solid $border-color;
+
+    .panel-title {
+      font-size: 1.6em;
+      @include text-extra-thin;
+    }
+
+    .panel-subtitle {
+      margin-top: 5px;
+      font-size: 0.9em;
+      @include text-extra-thin;
+      font-family: $font-family-alt;
+      color: $text-secondary;
+    }
+  }
+
+  .panel-body {
+    padding: $padding;
+  }
+}
+
+.text-field {
+  padding: 12px 15px;
+  font-size: 1em;
+  border-radius: 4px;//$border-radius;
+  // border: 1px solid #cecece;
+  outline: none;
+  @include text-extra-thin();
+  width: 100%;
+  display: block;
+  border: 0;
+  box-shadow: 0px 0px 0px 1px #cecece inset;
+
+  &:focus {
+    // border-color: #85a7ef;
+    // box-shadow: 0 0 0 2px rgba(81, 203, 238, 1) inset;
+    border: 0;
+    box-shadow: 0 0 0 2px #85a7ef inset;
+  }
+
+  resize: none;
+  &.resizable {
+    resize: vertical;
+  }
+
+  &::placeholder {
+    color: $text-secondary;
+  }
+}
+
+.button {
+  $btn-bgd: #cecece;
+  $btn-color: #555;
+  // padding: 8px 15px;
+  height: 36px;
+  padding: {
+    left: 15px;
+    right: 15px;
+  }
+  border: 1px solid $btn-bgd;
+  outline: none;
+  border-radius: 3px;
+  color: $btn-color;
+  background: white;
+
+  &:hover, &:active {
+    color: #85a7ef;
+    border: 1px solid #85a7ef;
+    
+  }
+
+  &:active {
+    box-shadow: 0 0 0 2px #b2c6f1;
+  }
+
+  &.fill {
+    color: white;
+    background-color: #4b83e6;
+    border: none;
+
+    &:hover, &:active {
+      background-color: #3d68b5;
+      // box-shadow: none;
+    }
+  }
+}
+
+.field-group {
+  margin-bottom: $padding;
 }
 
 .banner {
